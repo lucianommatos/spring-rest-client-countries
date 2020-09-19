b8f33d27c8a73565afd727cc984a0a0f740f7f62
lucianommatos <lucianommatos@gmail.com>
2020-09-19 14:33:23 +0100

    Added initial files

---
+++ src/main/java/com/project/springrestclientcountries/SpringRestClientCountriesApplication.java
@@ -0,0 +1,13 @@
+package com.project.springrestclientcountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientCountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestClientCountriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/springrestclientcountries/SpringRestClientCountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springrestclientcountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientCountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
