617323c5e1428e2e3b55a68ad61ff4f57082f7d0
lucianommatos <lucianommatos@gmail.com>
2020-09-19 14:58:14 +0100

    Added POJOs

---
+++ src/main/java/com/project/springrestclientcountries/model/Translations.java
@@ -0,0 +1,109 @@
+
+package com.project.springrestclientcountries.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Translations {
+
+    private String de;
+    private String es;
+    private String fr;
+    private String ja;
+    private String it;
+    private String br;
+    private String pt;
+    private String nl;
+    private String hr;
+    private String fa;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getDe() {
+        return de;
+    }
+
+    public void setDe(String de) {
+        this.de = de;
+    }
+
+    public String getEs() {
+        return es;
+    }
+
+    public void setEs(String es) {
+        this.es = es;
+    }
+
+    public String getFr() {
+        return fr;
+    }
+
+    public void setFr(String fr) {
+        this.fr = fr;
+    }
+
+    public String getJa() {
+        return ja;
+    }
+
+    public void setJa(String ja) {
+        this.ja = ja;
+    }
+
+    public String getIt() {
+        return it;
+    }
+
+    public void setIt(String it) {
+        this.it = it;
+    }
+
+    public String getBr() {
+        return br;
+    }
+
+    public void setBr(String br) {
+        this.br = br;
+    }
+
+    public String getPt() {
+        return pt;
+    }
+
+    public void setPt(String pt) {
+        this.pt = pt;
+    }
+
+    public String getNl() {
+        return nl;
+    }
+
+    public void setNl(String nl) {
+        this.nl = nl;
+    }
+
+    public String getHr() {
+        return hr;
+    }
+
+    public void setHr(String hr) {
+        this.hr = hr;
+    }
+
+    public String getFa() {
+        return fa;
+    }
+
+    public void setFa(String fa) {
+        this.fa = fa;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
